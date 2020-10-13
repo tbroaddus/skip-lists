98063fcc28842fe60ded3d144df471c40a58d45b
tbroaddus <dzt972@mocs.utc.edu>
2020-10-13 17:20:21 -0400

    Layering

--- cpp/src/Skip_List.h
+++ cpp/src/Skip_List.h
@@ -47,12 +47,15 @@ class Skip_List {
 
     // Helper private member function that takes in 
     // a shared_ptr reference to another element 
-    void add_element_helper(std::shared_ptr<Element<T>>&, const T&);
+    void add_element_helper(std::shared_ptr<Element<T>>, const T&);
 
     void add_layer(std::shared_ptr<Element<T>>&, const T&, const int);
 
+    void add_element_layer(std::shared_ptr<Element<T>>, std::shared_ptr<Element<T>>, const int layer);
+
     void print_layer() const;
 
+    // Private Member Variables
     std::array<std::shared_ptr<Element<T>>,N> head_arr;
     const int layers;
     int size;
@@ -93,11 +96,20 @@ Skip_List<T,N>& Skip_List<T,N>::operator=(Skip_List<T,N>&& _Skip_List) {
 template<class T, int N>
 void Skip_List<T,N>::add_element(const T& key) {
   if (head_arr.at(0)==nullptr) {
-    head_arr.at(0) = std::make_shared<Element<T>>(key);
+    for (int i = 0; i < N; i++) {
+      head_arr.at(i) = std::make_shared<Element<T>>(key);
+    }
+    for (int i = 0; i < N-1; i++) {
+      head_arr.at(i)->above = head_arr.at(i+1);
+      head_arr.at(i+1)->below = head_arr.at(i);
+    }
   } else if (key <= head_arr.at(0)->key) {
     head_arr.at(0)->prev = std::make_shared<Element<T>>(key);
     head_arr.at(0)->prev->next = head_arr.at(0);
     head_arr.at(0) = head_arr.at(0)->prev;
+    for (int i = 1; i < N; i++) {
+      head_arr.at(i) = std::make_shared<Element<T>>(key);
+    }
   } else if (head_arr.at(0)->next==nullptr) {
     head_arr.at(0)->next = std::make_shared<Element<T>>(key);
     head_arr.at(0)->next->prev = head_arr.at(0);
@@ -107,7 +119,7 @@ void Skip_List<T,N>::add_element(const T& key) {
 }
 
 template<class T, int N>
-void Skip_List<T,N>::add_element_helper(std::shared_ptr<Element<T>>& element_ptr, const T& key) { 
+void Skip_List<T,N>::add_element_helper(std::shared_ptr<Element<T>> element_ptr, const T& key) { 
   if (key <= element_ptr->key) {
     // TODO: For some reason this does not work... find out why!
     /*
@@ -120,13 +132,20 @@ void Skip_List<T,N>::add_element_helper(std::shared_ptr<Element<T>>& element_ptr
     std::shared_ptr<Element<T>> new_element = std::make_shared<Element<T>>(key);
     new_element->prev = element_ptr->prev;
     new_element->next = element_ptr;
-    new_element->prev->next=new_element;
-    element_ptr->prev = element_ptr;
+    element_ptr->prev->next = new_element;
+    element_ptr->prev = new_element;
+    if (new_element->next->next != nullptr){ 
+    std::cout << "At the end, " << new_element->key << " is pointing to " << new_element->next->key << " and " << new_element->next->key << " is pointing to " << new_element->next->next->key;
+    }
+    add_layer(new_element, key, 1);
   } else if (element_ptr->next == nullptr) {
-    element_ptr->next = std::make_shared<Element<T>>(key);
-    element_ptr->next->prev = element_ptr;
+    std::shared_ptr<Element<T>> new_element = std::make_shared<Element<T>>(key);
+    element_ptr->next = new_element; 
+    new_element->prev = element_ptr;
     //element_ptr = element_ptr->next;
     //std::cout << element_ptr->prev->key << std::endl;
+    add_layer(element_ptr->next, key, 1);
+    
   } else {
     add_element_helper(element_ptr->next, key);
   }
@@ -139,8 +158,35 @@ void Skip_List<T,N>::add_layer(std::shared_ptr<Element<T>>& element_ptr, const T
     return;
   else if (rand() % 2 == 0) {
     // Build Layer and recursively call add layer
+    std::shared_ptr<Element<T>> new_element = std::make_shared<Element<T>>(key);
+    new_element->below = element_ptr;
+    element_ptr->above = new_element;
+    add_element_layer(head_arr.at(layer), new_element, layer);
+    add_layer(new_element, key, layer+1);
   }
-  // 
+}
+
+template<class T, int N>
+void Skip_List<T,N>::add_element_layer(std::shared_ptr<Element<T>> element_ptr, std::shared_ptr<Element<T>> new_element, const int layer) {
+  if (element_ptr->next == nullptr) {
+    if (new_element->key >= element_ptr->key) {
+      element_ptr->next = new_element;
+      element_ptr->next->prev = element_ptr;
+    } else {
+      new_element->next = element_ptr;
+      new_element->prev = element_ptr->prev;
+      new_element->prev->next = new_element;
+      element_ptr->prev = new_element;
+    }
+  }
+   else if (new_element->key <= element_ptr->key) {
+    new_element->prev = element_ptr->prev;
+    new_element->next = element_ptr;
+    new_element->prev->next = new_element;
+    element_ptr->prev = new_element;
+  } else {
+    add_element_layer(element_ptr->next, new_element, layer);
+  }
 }
 
 template<class T, int N>
@@ -149,6 +195,7 @@ bool Skip_List<T,N>::contains(const T& key) {
   return false;
 }
 
+// TODO: increment size after additions
 template<class T, int N>
 int Skip_List<T,N>::list_size() const {
   return size;
@@ -166,14 +213,6 @@ void Skip_List<T,N>::print_layers() const {
     std::cout << "Layer " << i << ": ";
     std::shared_ptr<Element<T>> curr_element = head_arr.at(i);
     while(curr_element != nullptr) {
-      /*
-      if (curr_element->prev == nullptr) { 
-      std::cout << curr_element->key << ' ';
-      std::cout << std::endl;
-      } else {
-        std::cout << curr_element->prev->key << " " << curr_element->key << std::endl;
-      }
-      */
       std::cout << curr_element->key << ' ';
       curr_element = curr_element->next;
     }
--- cpp/src/test.cpp
+++ cpp/src/test.cpp
@@ -4,12 +4,21 @@
 
 int main(int argc, char** argv) {
 
-  Skip_List<int,3> skip_list;
-  skip_list.add_element(5);
-  skip_list.add_element(4);
-  skip_list.add_element(6);
+  Skip_List<int,5> skip_list;
+  skip_list.add_element(2);
+  skip_list.add_element(3);
   skip_list.add_element(8);
-  skip_list.add_element(5);
+  skip_list.add_element(9);
+  skip_list.add_element(39);
+  skip_list.add_element(11);
+ // skip_list.add_element(10);
+  skip_list.add_element(20);
+  skip_list.add_element(19);
   skip_list.add_element(7);
+  skip_list.add_element(340);
+  skip_list.add_element(6);
+  //skip_list.add_element(21);
+
+  //skip_list.add_element(10);
   skip_list.print_layers();
 }
